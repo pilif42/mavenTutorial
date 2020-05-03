af65d94ebc15e6eb47383142eb35ce8efb32de49
Philippe Brossier <brossierp@gmail.com>
2020-05-03 11:50:05 +0100

    msg now read from app.properties

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Greeting;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,9 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloWorldController {
-    private static final String template = "Hello, %s!";
+    @Value( "${templateMsg}" )
+    private String template;
+
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
